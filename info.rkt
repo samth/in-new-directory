f7020748288df28ed8371a521781a5d0986582a6
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-03-30 09:29:07 -0400

    Spell deps properly.

--- info.rkt
+++ info.rkt
@@ -2,4 +2,4 @@
 (define collection "file")
 (define scribblings '(("in-new-directory.scrbl")))
 (define deps '("base" "compatibility-lib"))
-(define build-desp '("scribble-lib"))
+(define build-deps '("scribble-lib"))
